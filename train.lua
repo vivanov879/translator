0e1a46c5c70d51273d1aa0e7a389c1784ef373d7
Владимир Иванов <vivanov879@ya.ru>
2015-06-28 23:14:54 +0300

    asdf

--- train.lua
+++ train.lua
@@ -6,11 +6,10 @@ require 'optim'
 require 'image'
 require 'Embedding'
 local model_utils=require 'model_utils'
-local mnist = require 'mnist'
 require 'table_utils'
 nngraph.setDebug(true)
 
-rnn_size = 100
+rnn_size = 4
 vocab_size = 10002
 
 --train data
@@ -146,49 +145,58 @@ function feval(x_arg)
     ------------------- forward pass -------------------
     lstm_c_enc = {[0]=torch.zeros(1, rnn_size)}
     lstm_h_enc = {[0]=torch.zeros(1, rnn_size)}
+    x_enc_embedding = {}
         
     local loss = 0
     
     x_enc = x_raw_enc[iteration_counter]
     for t = 1, x_enc:size(2) - 1 do
-      x_enc_embedding[t] = embed_enc_clones[t]:forward(x_enc[{{}, {t}}])
+      x_enc_embedding[t] = embed_enc_clones[t]:forward(x_enc[{{}, {t}}]:reshape(1))
       lstm_c_enc[t], lstm_h_enc[t] = unpack(encoder_clones[t]:forward({x_enc_embedding[t], lstm_c_enc[t-1], lstm_h_enc[t-1]}))
     end
     
     lstm_c_dec = {[0]=torch.zeros(1, rnn_size)}
-    lstm_h_dec = {[0]=lstm_h_enc[#x_enc-1]}
+    lstm_h_dec = {[0]=lstm_h_enc[x_enc:size(2)-1]}
     x_dec_prediction = {}
+     x_dec_embedding = {}
     
-    x_dec = x_raw_dec[iteration_counter]     
-    x_dec[0] = x_enc[t]
-    x_dec_embedding[0] = embed_dec_clones[0]:forward(x_dec[{{}, {0}}])
+    x_dec = x_raw_dec[iteration_counter]
+    x_dec_embedding[0] = embed_dec_clones[1]:forward(torch.Tensor(1):fill(vocab_size))
     for t = 1, x_dec:size(2) - 1 do 
-      x_dec_embedding[t] = embed_dec_clones[t]:forward(x_dec[{{}, {t}}])
+      x_dec_embedding[t] = embed_dec_clones[t]:forward(x_dec[{{}, {t}}]:reshape(1))
       lstm_c_dec[t], lstm_h_dec[t], x_dec_prediction[t] = unpack(decoder_clones[t]:forward({x_dec_embedding[t-1], lstm_c_dec[t-1], lstm_h_dec[t-1]}))
-      loss_x = criterion_clones[t]:forward(x_dec_prediction[t], x_dec_embedding[t])
+      loss_x = criterion_clones[t]:forward(x_dec_prediction[t], x_dec[{{}, {t}}]:reshape(1))
       loss = loss + loss_x
+      print(loss_x)
             
     end
     loss = loss / x_dec:size(2)
 
     ------------------ backward pass -------------------
     -- complete reverse order of the above
-    dlstm_c_dec = {[#(x_raw_dec[iteration_counter]) - 1] = torch.zeros(1, rnn_size)}
-    dlstm_h_dec = {[#(x_raw_dec[iteration_counter]) - 1] = torch.zeros(1, rnn_size)}
+    dlstm_c_dec = {[x_dec:size(2) - 1] = torch.zeros(1, rnn_size)}
+    dlstm_h_dec = {[x_dec:size(2) - 1] = torch.zeros(1, rnn_size)}
+    dx_dec_prediction = {}
+    dx_dec_embedding = {}
+    dx_dec = {}
     dloss_x = {}
     
-    for t = #(x_raw_dec[iteration_counter]) - 1,1,-1 do
-      dx_dec_prediction[t] = criterion_clones[t]:backward(x_dec_prediction[t], x_dec_embedding[t])
+    for t = x_dec:size(2) - 1,1,-1 do
+      dx_dec_prediction[t] = criterion_clones[t]:forward(x_dec_prediction[t], x_dec[{{}, {t}}]:reshape(1))
+      dx_dec_prediction[t] = torch.Tensor(1):fill(dx_dec_prediction[t])
       dx_dec_embedding[t-1], dlstm_c_dec[t-1], dlstm_h_dec[t-1] = unpack(decoder_clones[t]:backward({x_dec_embedding[t-1], lstm_c_dec[t-1], lstm_h_dec[t-1]}, {dlstm_c_dec[t], dlstm_h_dec[t], dx_dec_prediction[t]}))
-      dx_dec[{{}, {t}}] = embed_dec_clones[t]:backward(x_dec[{{}, {t}}], dx_dec_embedding[t])
+      dx_dec[t] = embed_dec_clones[t]:backward(x_dec[{{}, {t}}]:reshape(1), dx_dec_embedding[t-1])
     end
     
-    dlstm_c_enc = {[#x_enc - 1] = torch.zeros(1, rnn_size)}
-    dlstm_h_enc = {[#x_enc - 1] = dlstm_h_dec[0]}
+    dlstm_c_enc = {[x_enc:size(2) - 1] = torch.zeros(1, rnn_size)}
+    dlstm_h_enc = {[x_enc:size(2) - 1] = dlstm_h_dec[0]}
+    dx_enc_embedding = {}
+    dx_enc = {}
+
         
-    for t = #x_enc -1, 1, -1 do
+    for t = x_enc:size(2) -1, 1, -1 do
       dx_enc_embedding[t], dlstm_c_enc[t-1], dlstm_h_enc[t-1] = unpack(encoder_clones[t]:backward({x_enc_embedding[t], lstm_c_enc[t-1], lstm_h_enc[t-1]}, {dlstm_c_enc[t], dlstm_h_enc[t]}))
-      dx_enc[{{}, {t}}] = embed_enc_clones[t]:backward(x_enc[{{}, {t}}], dx_enc_embedding[t])
+      dx_enc[{{}, {t}}] = embed_enc_clones[t]:backward(x_enc[{{}, {t}}]:reshape(1), dx_enc_embedding[t])
     end
       
     -- clip gradient element-wise
@@ -200,6 +208,8 @@ function feval(x_arg)
 
     return loss, grad_params
 end
+
+optim_state = {learningRate = 1e-2}
 
 
 for i = 1, 1000 do
